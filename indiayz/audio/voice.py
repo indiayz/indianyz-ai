bec57599968e5c2de2a4634a3a9f9e636f5afb65
ꜱᴀʙᴋᴀ ʙʜᴀɪ <deepanshumeena529@gmail.com>
2026-01-09 12:23:17 +0530

    Update voice.py

--- indiayz/audio/voice.py
+++ indiayz/audio/voice.py
@@ -1,16 +1,6 @@
-# indiayz/audio/voice.py
-
 from indiayz.core.base import BaseModule
 
-
 class Voice(BaseModule):
-    """
-    Audio & voice interface.
-    """
-
-    name = "audio.voice"
-
     @staticmethod
     def tts(text: str):
-        response = Voice._post("/audio/tts", {"text": text})
-        return response
+        return Voice._post("/audio/tts", {"text": text})
