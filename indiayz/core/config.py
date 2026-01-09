6e4b7359ec15f0cf304c76807962bc2521201fd6
ꜱᴀʙᴋᴀ ʙʜᴀɪ <deepanshumeena529@gmail.com>
2026-01-09 17:00:59 +0530

    Update config.py

--- indiayz/core/config.py
+++ indiayz/core/config.py
@@ -1,4 +1,2 @@
-import os
-
-BASE_URL = os.getenv("BASE_URL", "https://indiayz-aebd81835b83.herokuapp.com")
-TIMEOUT = int(os.getenv("TIMEOUT", "60"))
+BASE_URL = "https://indiayz-aebd81835b83.herokuapp.com"
+TIMEOUT = 60
