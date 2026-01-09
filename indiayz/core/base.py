99b4b9e230258ed9078d0d5e7a4f4bd4db10ae71
ꜱᴀʙᴋᴀ ʙʜᴀɪ <deepanshumeena529@gmail.com>
2026-01-09 17:01:31 +0530

    Update base.py

--- indiayz/core/base.py
+++ indiayz/core/base.py
@@ -4,15 +4,13 @@ from indiayz.core.config import BASE_URL, TIMEOUT
 class BaseModule:
     @staticmethod
     def _post(endpoint: str, data: dict):
-        headers = {
-            "Content-Type": "application/json"
-        }
-
-        r = requests.post(
-            f"{BASE_URL}{endpoint}",
-            json=data,
-            headers=headers,
-            timeout=TIMEOUT
-        )
-        r.raise_for_status()
-        return r.json()
+        try:
+            r = requests.post(
+                f"{BASE_URL}{endpoint}",
+                json=data,
+                timeout=TIMEOUT
+            )
+            r.raise_for_status()
+            return r.json()
+        except Exception:
+            raise RuntimeError("Indiayz service is currently unavailable")
